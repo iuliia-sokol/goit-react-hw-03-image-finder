f72a5736f40e09cf93c47f725a9beaf8f6a94265
Iuliia Sokol <avare779@gmail.com>
2022-12-13 14:07:01 +0200

    components files created

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+  color: #010101;
+  padding: 40px;
+`;
